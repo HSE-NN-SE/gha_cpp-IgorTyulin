0759cf3bde8d9d053cfe5af89f73f300ea77ca39
IgorTyulin <57899934+IgorTyulin@users.noreply.github.com>
2021-04-09 13:08:29 +0300

    Update wok.cpp

--- wok.cpp
+++ wok.cpp
@@ -14,16 +14,6 @@ bool less<const char*>(const char* x, const char* y)
     return strcmp(x,y)<0;
 }
 
-template<typename T>
-T averageArr(T* tar, int size)
-{
-    T res = 0;
-    for(int i=0; i<size; i++)
-    {
-        res += tar[i];
-    }
-    return res/size;
-}
 int main()
 {
     int x = 1;
@@ -32,7 +22,5 @@ int main()
     std::cout << res << std::endl;
     bool result = less("minimum", "max");
     std::cout << result << std::endl;
-    int mass[size] = {1,2,3,4,5};
-    std::cout << averageArr(mass,size);
     return 0;
 }
