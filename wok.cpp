12a9498ee405751ecbb4150888808976653fb676
IgorTyulin <57899934+IgorTyulin@users.noreply.github.com>
2021-04-09 13:33:41 +0300

    Update wok.cpp

--- wok.cpp
+++ wok.cpp
@@ -19,8 +19,15 @@ int main()
     int x = 1;
     int y = 25;
     bool res = less(x,y);
+    if(res == 1)
+        std::cout << "X больше чем Y" << std::endl;
+    else
+        std::cout << "Y больше чем X" << std::endl;
     std::cout << res << std::endl;
     bool result = less("minimum", "max");
-    std::cout << result << std::endl;
+    if(result == 1)
+        std::cout << "Я выиграл 8" << std::endl;
+    else
+        std::cout << "Я всё ещё выиграл 8" << std::endl;
     return 0;
 }
