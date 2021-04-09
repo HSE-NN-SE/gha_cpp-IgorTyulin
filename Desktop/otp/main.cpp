609800fadbf429260af032ed645fec0d325b9698
IgorTyulin <57899934+IgorTyulin@users.noreply.github.com>
2021-04-09 12:32:37 +0300

    Update main.cpp

--- Desktop/otp/main.cpp
+++ Desktop/otp/main.cpp
@@ -1,6 +1,29 @@
-#include "task1.h"
+#include <iostream>
 #define size 5
+#include <string.h>
 
+template<typename T>
+bool less(T x, T y)
+{
+    return x<y;
+}
+
+template<>
+bool less<const char*>(const char* x, const char* y)
+{
+    return strcmp(x,y)<0;
+}
+
+template<typename T>
+T averageArr(T* tar, int size)
+{
+    T res = 0;
+    for(int i=0; i<size; i++)
+    {
+        res += tar[i];
+    }
+    return res/size;
+}
 int main()
 {
     int x = 1;
